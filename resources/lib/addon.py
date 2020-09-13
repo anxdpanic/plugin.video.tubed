3ef0a3dab959f60e1046f916f449c5fca2869458
anxdpanic <anxdpanic@users.noreply.github.com>
2020-09-13 12:32:06 -0400

    Initial commit

---
+++ resources/lib/addon.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2020 Tubed (plugin.video.tubed)
+
+    This file is part of plugin.video.tubed
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+    See LICENSES/GPL-2.0-or-later.txt for more information.
+"""
