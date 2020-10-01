e70282d9daeb1105976813fa42ff8f364cce16aa
anxdpanic <anxdpanic@users.noreply.github.com>
2020-10-01 12:36:51 -0400

    add video rating

---
+++ resources/lib/src/scripts/rate.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2020 Tubed (plugin.video.tubed)
+
+    This file is part of plugin.video.tubed
+
+    SPDX-License-Identifier: GPL-2.0-only
+    See LICENSES/GPL-2.0-only.txt for more information.
+"""
+
+from urllib.parse import unquote
+
+import xbmc  # pylint: disable=import-error
+import xbmcgui  # pylint: disable=import-error
+
+from ..lib.memoizer import reset_cache
+
+
+def invoke(context, video_id, video_title=''):
+    if video_title:
+        try:
+            video_title = unquote(video_title)
+        except:  # pylint: disable=bare-except
+            pass
+
+    payload = context.api.rating(video_id)
+    try:
+        payload = payload.get('items', [{}])[0]
+    except IndexError:
+        return
+
+    rating = payload.get('rating', 'none')
+
+    choices = [
+        context.i18n('I like this'),
+        context.i18n('I dislike this'),
+        context.i18n('Remove rating')
+    ]
+
+    choice_map = [
+        'like',
+        'dislike',
+        'none'
+    ]
+
+    del choices[choice_map.index(rating)]
+    del choice_map[choice_map.index(rating)]
+
+    result = xbmcgui.Dialog().select(context.i18n('Rate'), choices)
+    if result == -1:
+        return
+
+    rating = choice_map[result]
+
+    payload = context.api.rate(video_id, rating)
+
+    try:
+        successful = int(payload.get('error', {}).get('code', 204)) == 204
+    except ValueError:
+        successful = False
+
+    if successful:
+        message = ''
+
+        if video_title:
+            if rating == 'none':
+                message = context.i18n('Rating removed from %s') % video_title
+
+            elif rating == 'like':
+                message = context.i18n('Liked %s') % video_title
+
+            elif rating == 'dislike':
+                message = context.i18n('Disliked %s') % video_title
+
+        else:
+            if rating == 'none':
+                message = context.i18n('Rating removed')
+
+            elif rating == 'like':
+                message = context.i18n('Liked')
+
+            elif rating == 'dislike':
+                message = context.i18n('Disliked')
+
+        xbmcgui.Dialog().notification(
+            context.addon.getAddonInfo('name'),
+            message,
+            context.addon.getAddonInfo('icon'),
+            sound=False
+        )
+
+        xbmc.executebuiltin('Container.Refresh')
+        reset_cache()
