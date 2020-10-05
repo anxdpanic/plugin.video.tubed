ec3c8802f54ff90408e5fd92237b2f2f6889bb68
anxdpanic <anxdpanic@users.noreply.github.com>
2020-10-04 20:00:51 -0400

    redirect play script endpoint to avoid duplicate code

--- resources/lib/src/scripts/play.py
+++ resources/lib/src/scripts/play.py
@@ -8,37 +8,8 @@
     See LICENSES/GPL-2.0-only.txt for more information.
 """
 
-import xbmc  # pylint: disable=import-error
-
-from ..api.utils import choose_subtitles
-from ..items.stream import Stream
+from ..routes import play
 
 
 def invoke(context, video_id, prompt_subtitles=False):
-    quality = context.api.quality(
-        context.settings.video_quality,
-        limit_30fps=context.settings.limit_to_30fps,
-        hdr=context.settings.hdr
-    )
-
-    video = context.api.resolve(video_id=video_id, quality=quality)
-    channel = video.get('channel', {})
-    license_data = video.get('license', {})
-    metadata = video.get('metadata', {})
-
-    stream = Stream(
-        label=metadata.get('title', ''),
-        label2=channel.get('author', ''),
-        path=video.get('url', ''),
-        headers=video.get('headers', ''),
-        license_key=license_data.get('proxy', '')
-    )
-
-    subtitles = choose_subtitles(
-        context,
-        subtitles=metadata.get('subtitles', []),
-        prompt_override=prompt_subtitles
-    )
-    stream.ListItem.setSubtitles(subtitles)
-
-    xbmc.Player().play(item=stream.ListItem.getPath(), listitem=stream.ListItem)
+    play.invoke(context, video_id, prompt_subtitles)
