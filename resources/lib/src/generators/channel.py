870666313b1187ca5f6118e56a9b013f2072d09c
anxdpanic <anxdpanic@users.noreply.github.com>
2020-10-13 10:48:49 -0400

    fixup channel generator

--- resources/lib/src/generators/channel.py
+++ resources/lib/src/generators/channel.py
@@ -44,11 +44,10 @@ def channel_generator(context, items):
         if not snippet:
             continue
 
-        channel_name = unescape(snippet.get('channelTitle', ''))
+        channel_name = unescape(snippet.get('title', ''))
 
         payload = Directory(
-            label=unescape(snippet.get('title', '')),
-            label2=channel_name,
+            label=channel_name,
             path=create_addon_path({
                 'mode': str(MODES.CHANNEL),
                 'channel_id': channel_id
@@ -58,8 +57,8 @@ def channel_generator(context, items):
         info_labels = {
             'plot': unescape(snippet.get('description', '')),
             'plotoutline': unescape(snippet.get('description', '')),
-            'originaltitle': unescape(snippet.get('title', '')),
-            'sorttitle': unescape(snippet.get('title', '')),
+            'originaltitle': channel_name,
+            'sorttitle': channel_name,
             'studio': channel_name
         }
         payload.ListItem.setInfo('video', info_labels)
