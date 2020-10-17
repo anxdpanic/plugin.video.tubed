feb76c4c343d1d5db330477c2e553732bf5d48d8
anxdpanic <anxdpanic@users.noreply.github.com>
2020-10-17 13:15:17 -0400

    add more metadata to autoplay related

--- resources/lib/src/dialogs/utils.py
+++ resources/lib/src/dialogs/utils.py
@@ -11,17 +11,19 @@
 import json
 from html import unescape
 
+import arrow
 import xbmc  # pylint: disable=import-error
 
 from ..generators.data_cache import get_cached
 from ..generators.utils import get_thumbnail
 from ..generators.video import video_generator
 from ..lib.logger import Log
+from ..lib.time import iso8601_duration_to_seconds
 
 LOG = Log('dialogs', __file__)
 
 
-def add_related_video_to_playlist(context, video_id):
+def add_related_video_to_playlist(context, video_id):  # pylint: disable=too-many-locals
     playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
     metadata = {}
 
@@ -62,14 +64,59 @@ def add_related_video_to_playlist(context, video_id):
                 cached_payload = get_cached(context, context.api.videos, [related_id])
                 cached_video = cached_payload.get(related_id, {})
                 cached_snippet = cached_video.get('snippet', {})
+                cached_content_details = cached_video.get('contentDetails', {})
+                cached_statistics = cached_video.get('statistics', {})
 
-                metadata.update({
+                channel_id = cached_snippet.get('channelId', '')
+                channel_name = unescape(cached_snippet.get('channelTitle', ''))
+
+                video_title = unescape(cached_snippet.get('title', ''))
+
+                published_arrow = None
+                live_details = cached_video.get('liveStreamingDetails')
+
+                if live_details:
+                    actual_start = live_details.get('actualStartTime')
+                    actual_end = live_details.get('actualEndTime')
+                    scheduled_start = live_details.get('scheduledStartTime')
+
+                    published = actual_end or actual_start or scheduled_start
+                    if published:
+                        published_arrow = arrow.get(published).to('local')
+
+                if not published_arrow:
+                    published_arrow = arrow.get(cached_snippet['publishedAt']).to('local')
+
+                likes = int(cached_statistics.get('likeCount', '0'))
+                dislikes = int(cached_statistics.get('dislikeCount', '0'))
+                votes = likes + dislikes
+                try:
+                    rating = '%0.1f' % ((likes / votes) * 10)
+                except ZeroDivisionError:
+                    rating = '0.0'
+
+                metadata = {
                     'video_id': related_id,
-                    'title': unescape(cached_snippet.get('title', '')),
+                    'channel_id': channel_id,
+                    'title': video_title,
                     'description': unescape(cached_snippet.get('description', '')),
-                    'channel_name': unescape(cached_snippet.get('channelTitle', '')),
-                    'thumbnail': get_thumbnail(cached_snippet)
-                })
+                    'channel_name': channel_name,
+                    'year': published_arrow.year,
+                    'premiered': published_arrow.format('YYYY-MM-DD'),
+                    'dateadded': published_arrow.format('YYYY-MM-DD HH:mm:ss'),
+                    'tag': cached_snippet.get('tags', ''),
+                    'rating': rating,
+                    'votes': votes,
+                    'like_count': likes,
+                    'dislike_count': dislikes,
+                    'view_count': int(cached_statistics.get('viewCount', 0)),
+                    'comment_count': int(cached_statistics.get('commentCount', 0)),
+                    'thumbnail': get_thumbnail(cached_snippet),
+                }
+
+                duration = iso8601_duration_to_seconds(cached_content_details.get('duration', ''))
+                if duration:
+                    metadata['duration'] = duration
 
                 generated = list(video_generator(context, [add_item]))
                 path, list_item, _ = generated[0]
